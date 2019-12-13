5c118f3937b735f66bd56842d5a13e896008b6ce
smy <1012699841@qq.com>
2019-12-13 11:11:14 +0800

    all is new

---
+++ src/main/java/com/shsxt/crm/controller/CustomerController.java
@@ -0,0 +1,8 @@
+package com.shsxt.crm.controller;
+
+import com.shsxt.base.BaseController;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CustomerController extends BaseController {
+}
--- src/main/java/com/shsxt/crm/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.shsxt.crm.service;
-
-import com.shsxt.base.BaseService;
-import com.shsxt.crm.vo.Customer;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CustomerService extends BaseService<Customer,Integer> {
-}
